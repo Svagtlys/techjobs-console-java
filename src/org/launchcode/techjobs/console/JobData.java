5921fe763e63771a2954bf0dfc6530da1026bc56
Svagtlys <45446348+Svagtlys@users.noreply.github.com>
2020-01-06 19:55:40 -0600

    Completed graded assignment requirements (printJobs, findByValue, case-insensitive search)

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -76,7 +76,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -123,6 +123,30 @@ public class JobData {
             System.out.println("Failed to load job data");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Searches for a string in all columns
+     */
+    public static ArrayList<HashMap<String, String>> findByValue(String value)
+    {
+        //Make sure data is loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> matchingJobs = new ArrayList<>();
+
+        for (HashMap<String, String> job : allJobs)
+        {
+            for(String key : job.keySet())
+            {
+                if(job.get(key).toLowerCase().contains(value.toLowerCase()))
+                {
+                    matchingJobs.add(job);
+                    break;
+                }
+            }
+        }
+        return matchingJobs;
     }
 
 }
